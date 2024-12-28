7aaa85eb955c7046e6effa6e18a178e6ebd7c04b
suhirdha24 <suhirdhasivakumar@gmail.com>
2024-12-28 11:41:21 +0530

    Adding operators.js

---
+++ operators.js
@@ -0,0 +1,70 @@
+//Assignment 
+//a=10;
+//console.log(a);
+
+//arithmetic operator
+
+//a=10;
+//b=2;
+//console.log("Add",a+b);
+//console.log("Sub",a-b);
+//console.log("Division",a/b);
+//console.log("Modulo",a/b);
+
+//relational operator
+
+//a=10;
+//if(a>20){
+//console.log("Greater than the given value");
+//}
+//else{
+// console.log("Lesser than the given value");
+//}
+
+//b=10;
+//if(b<=12){
+  //  console.log("Lesser");
+//}
+//else{
+  //  console.log("Greater");
+//}
+
+//c=12;
+//if(c==1){
+   // console.log("Equal");
+//}
+//else{
+   // console.log("Not equal");
+//}
+
+//c=12;
+//if(c===12){
+  //  console.log("Equal");
+//}
+//else{
+  //  console.log("Not equal");
+//}
+
+//Logical operator
+//a=10;
+//b=3;
+//console.log("AND",a&&b);
+//console.log("OR",a||b);
+//console.log("NOT",!a);
+
+//bitwise operator
+//a=5;
+//console.log(a&1);
+//console.log(a|2);
+//console.log(a<<2);
+//console.log(a>>2);
+
+//ternary operator
+//age=19;
+//let vote = (age < 18) ? "cant able to  vote" : "Can able to vote";
+//console.log(vote);
+
+//unary operator
+a=5;
+console.log(a++);
+console.log(a--);
